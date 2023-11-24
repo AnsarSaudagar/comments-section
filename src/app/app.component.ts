7a33adf6715d6a17aa7b8de4d0f73bed10e7501b
ansar <ansarsaudagar40@gmail.com>
2023-11-24 17:18:47 +0530

    all comments fetched

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,26 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { DataService } from './services/data.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, HttpClientModule],
+  providers:[DataService],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'interactive-comments-section';
+
+  constructor(private dataService : DataService){}
+
+  ngOnInit(){
+    this.dataService.getAllComments().subscribe(data=>{
+      console.log(data);
+      
+    })
+  }
 }
